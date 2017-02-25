56064d03eb058f974ca045d7729af41c9df5295f
Yilin Qin <yilinqin@Yilins-MacBook-Pro.local>
2017-02-25 11:41:35 -0600

    accepted sol

--- 407_trapping_rain_water_ii/sol.py
+++ 407_trapping_rain_water_ii/sol.py
@@ -2,8 +2,22 @@
 """
 Created on Fri Feb 24 19:11:11 2017
 
+Starting from the edge and go towards inside
+The reserver is bounded by the height of edge
+Think this as pull water from the edge to fill the area till hitting all higher bars
+Then repeat this again insdie the higher bars
+
+start from the edge
+put point into a priority queue sorted by height asc
+maintain the current water level starting from 0
+for each point not visited and inside the edge from the queue 
+  the reserve is waterlvl - height
+  add its 4 neighbors to the queue
+
 @author: yilinqin
 """
+
+import heapq
 
 class Solution(object):
     def trapRainWater(self, heightMap):
@@ -12,52 +26,42 @@ class Solution(object):
         :rtype: int
         """
         nrow = len(heightMap)
+        if nrow==0: return 0
         ncol = len(heightMap[0])
-        leftbar = [[] for i in range(nrow)]
-        rightbar = [[] for i in range(nrow)]
-        upbar = [[] for i in range(nrow)]
-        bottombar = [[] for i in range(nrow)]
-
+        if ncol==0: return 0
+        visitMap = [[] for i in range(nrow)]
+        #waterMap = [[] for i in range(nrow)]
+        pq = []
+        
         for i in range(nrow):
             for j in range(ncol):
-                leftbar[i].append(0)
-                rightbar[i].append(0)
-                upbar[i].append(0)
-                bottombar[i].append(0)
-
-        for i in range(nrow):
-            imax1 = -1
-            imax2 = -1
-            for j in range(ncol):                
-                if heightMap[i][j] > imax1:
-                    imax1 = heightMap[i][j]
-                leftbar[i][j] = imax1
-                if heightMap[i][ncol-1-j] > imax2:
-                    imax2 = heightMap[i][ncol-1-j]
-                rightbar[i][ncol-1-j] = imax2
-                    
-        for j in range(ncol):
-            imax1 = -1
-            imax2 = -1
-            for i in range(nrow): 
-                if heightMap[i][j] > imax1:
-                    imax1 = heightMap[i][j]
-                upbar[i][j] = imax1
-                if heightMap[nrow-1-i][j] > imax2:
-                    imax2 = heightMap[nrow-1-i][j]
-                bottombar[nrow-1-i][j] = imax2
+                visitMap[i].append(0)
+                #waterMap[i].append(0)
+                if i==0 or i==nrow-1 or j==0 or j==ncol-1:
+                    heapq.heappush(pq, (heightMap[i][j],(i,j)))    
                 
+        waterlv = 0
         res = 0
-        for i in range(1,nrow-1,1):
-            for j in range(1,ncol-1,1):
-                h = min(leftbar[i][j],rightbar[i][j],upbar[i][j],bottombar[i][j])
-                if heightMap[i][j]<h:                
-                    res = res + h - heightMap[i][j]
-        
-        print leftbar
-        print rightbar
-        print upbar
-        print bottombar
+        mv = ((0,-1),(0,1),(1,0),(-1,0))
+        while pq:
+            (val, (icur, jcur)) = heapq.heappop(pq)
+            if (visitMap[icur][jcur]==1): 
+                continue
+            else:
+                visitMap[icur][jcur]= 1
+                              
+            for (imv, jmv) in mv:
+                inxt = imv+icur
+                jnxt = jmv+jcur
+                if inxt>=0 and inxt<=nrow-1 and jnxt>=0 and jnxt<=ncol-1:
+                    heapq.heappush(pq, (heightMap[inxt][jnxt],(inxt,jnxt)))
+    
+            waterlv = max(waterlv, heightMap[icur][jcur])
+            if icur>0 and icur<nrow-1 and jcur>0 and jcur<ncol-1:
+                #waterMap[icur][jcur] = waterlv - heightMap[icur][jcur]
+                res = res + waterlv - heightMap[icur][jcur]
+                
+        #print (waterMap)
         return res
     
 
